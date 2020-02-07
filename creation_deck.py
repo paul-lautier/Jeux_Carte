0e94f5e979a19e906311c43f648a246aac515d97
lautier Paul <paul.lautier64@gmail.com>
2020-02-07 20:38:12 +0100

    del initialisation carte + finalisation de la création d'un deck

--- creation_deck.py
+++ creation_deck.py
@@ -1,7 +1,7 @@
-from initialisation_carte import *
-deck_joueur1 = []
-deck_joueur2 = []
-deck_complet = []
+
+import random
+import json
+
 
 
 class Carte():
@@ -12,7 +12,7 @@ class Carte():
         self.name_monstre = name_monstre
         self.sptite_path = sprite_path
 
-    def __rept__(self):
+    def __repr__(self):
         return "{}".format(self.name_monstre)
 
 
@@ -20,49 +20,34 @@ class Carte():
 
 
 
-def cree_carte():
-    deck = []
+def cree_deck():
+    deck1 = []
+    deck2 = []
     with open('data.json') as fichier:
         data = json.load(fichier)
-        for card_id, card_data in data.items():
-            carte = Carte(
-                **card_data
-                # card_data['dmg_monstre'],
-                # card_data['hp_monstre'],
-                # card_data['name_monstre'],
-                # card_data['hp_4invoc'],
-                # card_data['sprite_path']
-
-            )
-            
-
-    
-     
+        for carte_id, card_data in data.items():
+            carte = Carte(**card_data)
+            deck1.append(carte)
+            deck2.append(carte)
+        melange_deck(deck1,deck2)
+        test_deck(deck1,deck2)
 
 
+        
 
-def ajouter_au_deck():
-    for i in range (20) :
-        deck_joueur1.append(deck_complet)
-        deck_joueur2.append(deck_complet)
+def melange_deck(deck1,deck2):
+    random.shuffle(deck1)
+    random.shuffle(deck2)
 
+def test_deck(deck1,deck2):
+    print(deck1)
+    print(len(deck1))
 
-
-def melange_deck(deck_joueur1,deck_joueur2):
-    pass
-
-#founction de test des deck
-def test_deck():
-    # print(deck_joueur1)
-    # print(len(deck_joueur1))
-    # # print(deck_joueur2)
-    # print(len(deck_joueur2))
-    print(deck_complet)
-    # print(len(deck_complet))
+    print(deck2)
+    print(len(deck2))
 
 
-cree_carte()
-ajouter_au_deck()
-test_deck()
 
+
+cree_deck()
 
